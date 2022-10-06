7aca895c00434c129ab7c56e7efccf2663280d2f
Maria Chuikina <maria.chuikina@gmail.com>
2022-10-06 15:03:18 +0200

    Check token jwt

--- auth/auth.js
+++ auth/auth.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
 const UserModel = require('../model/model');
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExrtactJWT = require('passport-jwt').ExtractJwt;
+
+passport.use( new JWTstrategy({
+    secretOrKey: 'TOP_SECRET',
+    jwtFromRequest: ExrtactJWT.fromUrlQueryParameter('secret_token')
+    },
+    async (token, done) => {
+        try {
+            return done(null, token.user);
+        } catch (error) {
+            done(error);
+        }}));
 
 passport.use('signup', new localStrategy({
     usernameField: 'email',
